c7d0ddf2d0f24f99885a99c853e4ab0e13fe640f
Tijana Savic <tijanasavic1984@gmail.com>
2022-09-25 21:14:11 +0200

    added key code event

---
+++ main.js
@@ -0,0 +1,19 @@
+const code = document.querySelector("#code");
+
+window.addEventListener("keydown", (e) => {
+    console.log(e);
+    code.innerHTML = `
+    <div class="key">
+        ${e.key === " " ? "Space" : e.key}
+        <small>event.key</small>
+      </div>
+      <div class="key">
+        ${e.keyCode}
+        <small>event.keyCode</small>
+    </div>
+      <div class="key">
+        ${e.code} 
+        <small>event.code</small>
+    </div>
+`;
+});
